ab55b849f3ffacc37bcddf5701848f41837ff98a
aqlan-hussin <72571835+aqlan-hussin@users.noreply.github.com>
2020-11-09 14:15:02 -0500

    Second commit PEs

---
+++ PE12/pe12.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "bmp.h"
+int main(int argc, char *argv[])
+{
+    if (argc != 3) {
+        fprintf(stderr, "Wrong number of arguments\n");
+        return EXIT_FAILURE;
+    }
+
+    FILE* input = fopen(argv[1], "r");
+    if (input == NULL) {
+        fprintf(stderr, "Can't open input file\n");
+        return EXIT_FAILURE;
+    }
+
+    BMP_Image* image = Read_BMP_Image(input);
+
+    if (image == NULL) {
+        fprintf(stderr, "Can't read image from file\n");
+        fclose(input);
+        return EXIT_FAILURE;
+    }
+    
+    // ayah tambah ni .. should not use same variable for input and output
+    BMP_Image *outputImage;
+
+    if (image->header.bits == 24) {
+        outputImage = Convert_24_to_16_BMP_Image(image);
+    }
+
+    if (image->header.bits == 16) {
+        outputImage = Convert_16_to_24_BMP_Image(image);
+    }
+
+    FILE* output = fopen(argv[2], "w");
+    if (output == NULL) {
+        fprintf(stderr, "Can't open file for output\n");
+        fclose(input);
+        return EXIT_FAILURE;
+    }
+    
+
+    if (Write_BMP_Image(output, outputImage) == FALSE) {
+        fprintf(stderr, "Error writing to output file\n");
+        fclose(input);
+        fclose(output);
+        return EXIT_FAILURE;
+    }
+
+    Free_BMP_Image(image);
+    Free_BMP_Image(outputImage);
+    fclose(input);
+    fclose(output);
+    
+    return EXIT_SUCCESS;
+}
